aef78ab5f706b1409bb807229ee1f0614df11938
teruto725 <akiteruto@gmail.com>
2021-12-12 01:50:12 +0900

    rewrite App.test.tsx

---
+++ src/App.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { AnyAction, configureStore, Store } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "services/user/user";
+
+describe("XXX Component Test Cases", () => {
+  // テスト用のstore
+  let store: Store<any, AnyAction>;
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        user: userReducer,
+      },
+    });
+  });
+
+  it("テストケース1", async () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    // screen.debug();
+  });
+});

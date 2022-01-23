2e3eff4a72e2ac88d0a22021bbd6d04fcafb9a14
Akihito Ihara <akiteruto@gmail.com>
2022-01-23 13:59:17 +0900

    planetcomponentの作成

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { PlanetPicture } from "../../components/PlanetPicture/PlanetPicture";
 
 type Prop = {};
 
 export const Start: React.FC<Prop> = () => {
-  return <div>これはスタート画面です。</div>;
+  return (
+    <>
+      <div>これはスタート画面です。</div>
+      <PlanetPicture size={161} color="yellow" />
+    </>
+  );
 };

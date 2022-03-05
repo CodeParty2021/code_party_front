34f02ce438150611d05d5130b6cc6fcf149722f2
teruto725 <akiteruto@gmail.com>
2022-03-06 04:08:52 +0900

    fix typo

--- src/pages/Code/Coding/Coding.test.tsx
+++ src/pages/Code/Coding/Coding.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { CodeCording } from "./Cording";
+import { CodeCording } from "./Coding";
 
 describe("<CodeCording />", () => {
   it("auth snapshot test", () => {
--- src/pages/Code/Coding/Coding.tsx
+++ src/pages/Code/Coding/Coding.tsx
@@ -5,10 +5,8 @@ type Props = {};
 export const CodeCording: React.FC<Props> = () => {
   return (
     <div>
-      <div>コーディング画面</div>
-      <div>
-        <button>登録する</button>
-      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 };

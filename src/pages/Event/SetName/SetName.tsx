d9ea520c69416bc20fb6a07b71bd980ce6e78834
kacg-d <vitanexus7@gmail.com>
2022-05-18 20:27:40 +0900

    とりあえず表示できた

--- src/pages/Event/SetName/SetName.tsx
+++ src/pages/Event/SetName/SetName.tsx
@@ -1,23 +1,50 @@
+import { Button } from "components/Button/Button";
 import React from "react";
 import { useSetNameState } from "./hooks/useSetNameState";
-import { ModalTitle } from "./SetNameStyle";
+import {
+  AlgoBox,
+  AlgoHeadMini,
+  Message,
+  ModalTitle,
+  NameInput,
+} from "./SetNameStyle";
 import { Balloon, SetNameModal, SetNameStyle } from "./SetNameStyle";
 
 type Props = {};
 
 export const EventSetName: React.FC<Props> = () => {
-  const { loading, nameInputRef, startBtnHandler } = useSetNameState();
+  const {
+    loading,
+    nameInputRef,
+    startBtnHandler,
+    nameInputHandler,
+    btnDisabled,
+  } = useSetNameState();
   if (loading) {
     return <div>ロード中</div>;
   }
   return (
     <SetNameStyle>
       <SetNameModal>
-        <ModalTitle>はじめて遊ぶ</ModalTitle>
-        <Balloon src="/img/balloon.svg">名前を教えてほしい！</Balloon>
-
-        <input ref={nameInputRef} type="text"></input>
-        <button onClick={startBtnHandler}>けってい</button>
+        <ModalTitle>
+          <span>はじめて遊ぶ</span>
+        </ModalTitle>
+        <AlgoBox>
+          <AlgoHeadMini />
+          <Balloon src="/img/balloon.svg" />
+          <Message>名前を教えてほしい！</Message>
+        </AlgoBox>
+        <NameInput
+          ref={nameInputRef}
+          type="text"
+          onChange={nameInputHandler}
+        ></NameInput>
+        <Button
+          onClick={startBtnHandler}
+          color="pink"
+          value="けってい"
+          status={btnDisabled ? "disabled" : "default"}
+        />
       </SetNameModal>
     </SetNameStyle>
   );

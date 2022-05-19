1504cda0d03f40ef211fa2cba5bf460ad491f52d
kacg-d <vitanexus7@gmail.com>
2022-05-19 14:39:13 +0900

    story追加

--- src/components/StarBackground/StarBackground.stories.tsx
+++ src/components/StarBackground/StarBackground.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { StarBackground } from "./StarBackground";
+import { AlgoHead } from "components/Character/Algo/Head/AlgoHead";
 
 export default {
   title: "components/StarBackground",
@@ -14,3 +15,12 @@ const Template: ComponentStory<typeof StarBackground> = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {};
+
+const withComponentTemplate: ComponentStory<typeof StarBackground> = (args) => (
+  <StarBackground {...args}>
+    <AlgoHead />
+  </StarBackground>
+);
+
+export const withComponent = withComponentTemplate.bind({});
+withComponent.args = {};

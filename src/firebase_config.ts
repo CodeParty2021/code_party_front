157770bbf1266f9b3d638445c64e10f8768ccbf9
Takuma.S <baaninreo@gmail.com>
2022-07-21 05:06:51 +0900

    ローカルエミュレータを使用するように変更

--- src/firebase_config.ts
+++ src/firebase_config.ts
@@ -1,8 +1,11 @@
+// TODO:compat系はv8のライブラリをv9で動かす為のものなので、v8系の処理をなくせたら消したい
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
+import { getApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 import { firebaseConfig } from "config";
-import { getAuth } from "firebase/auth";
 
 //firebaseの初期化
 firebase.initializeApp(firebaseConfig);
@@ -10,3 +13,8 @@ firebase.initializeApp(firebaseConfig);
 export const getIdToken = async () => {
   return getAuth().currentUser?.getIdToken();
 };
+
+if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+  connectAuthEmulator(getAuth(), "http://localhost:9099");
+  connectDatabaseEmulator(getDatabase(getApp()), "localhost", 9000);
+}

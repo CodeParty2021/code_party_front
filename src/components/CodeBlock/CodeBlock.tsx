395c5d06e84cec7509b24c6717c0a33813c500b4
Shuhei Kayawari <35371161+wowry@users.noreply.github.com>
2022-09-02 21:09:32 +0900

    CodeBlockを追加 (#108)
    * add: CodeBlockを追加
    
    * chore: リファクタリング
    
    * fix: gapを削除
    
    * fix: スタイリングを修正
    
    * change: height上限を削除

---
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { CodeBlockStyle, editorStyleProps } from "./CodeBlockStyle";
+import Editor, { OnMount, EditorProps } from "@monaco-editor/react";
+
+type Props = {
+  code: string;
+  fontSize?: number;
+  height?: number; // 省略時はコンテンツの高さに自動調整
+  editorProps?: EditorProps;
+};
+
+export const CodeBlock: React.FC<Props> = ({
+  code = "",
+  fontSize = 18,
+  height = 0,
+  editorProps,
+  ...styleProps
+}) => {
+  const [editorHeight, setEditorHeight] = useState<number>(0);
+
+  const handleEditorDidMount: OnMount = (editor) => {
+    const contentHeight = editor.getContentHeight();
+    setEditorHeight(height || contentHeight);
+    editor.layout({ width: editor.getContentWidth(), height: editorHeight });
+
+    // "Cannot edit in read-only editor"のツールチップを非表示にする
+    const messageContribution = editor.getContribution(
+      "editor.contrib.messageController"
+    );
+    editor.onDidAttemptReadOnlyEdit(() => {
+      messageContribution?.dispose();
+    });
+  };
+
+  return (
+    <CodeBlockStyle {...styleProps}>
+      <Editor
+        className="CodeBlock"
+        {...editorStyleProps({ fontSize, editorHeight })}
+        defaultLanguage="python"
+        loading=""
+        value={code}
+        onMount={handleEditorDidMount}
+        {...editorProps}
+      />
+    </CodeBlockStyle>
+  );
+};
